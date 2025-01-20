8b62274951a57423da0fbe0491a4923d66190766
Ismael Gómez Brenes <r.ismael09@gmail.com>
2025-01-20 03:13:32 -0600

    actualizaciones

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export', 
+  basePath: '/Portafolios', 
+  assetPrefix: '/Portafolios/', 
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
